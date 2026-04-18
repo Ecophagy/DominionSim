05d87cdd22c920172fe54b16ddcfc08d513ae089
Ecophagy <oghill@gmail.com>
2026-04-18 15:48:18 +0100

    Mark base Simulator as ABC

--- models/simulator.py
+++ models/simulator.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
 from models.board import Board
 
 
-class Simulator:
+class Simulator(ABC):
+    @abstractmethod
     def __init__(self, board: Board):
         self.board = board
         self.runs = 0
@@ -9,9 +11,11 @@ class Simulator:
     def reset(self):
         self.board = Board()
 
+    @abstractmethod
     def run(self):
         self.reset()
         self.runs += 1
 
+    @abstractmethod
     def log_result(self):
         raise NotImplementedError
